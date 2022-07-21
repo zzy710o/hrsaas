f6fa21b77bd45cb5261a6d759f7f8faf9b12cefa
zzy710 <2714001407@qq.com>
2022-07-21 11:07:17 +0800

    人力资源初始化

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,13 @@
 module.exports = {
-
-  title: 'Vue Admin Template',
-
+  title: "Vue Admin Template",
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
   fixedHeader: false,
-
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false
-}
+  sidebarLogo: false,
+};
